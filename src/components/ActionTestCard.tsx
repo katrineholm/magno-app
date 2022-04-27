5d411435786fd6ae5681ab911783e943fb79c109
Jack Syvertsen <jmsyvert@stud.ntnu.no>
2022-04-27 10:41:29 +0200

    Adds tests page with action test cards

--- src/components/ActionTestCard.tsx
+++ src/components/ActionTestCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { CardActionArea, CardHeader, Grid } from '@material-ui/core/'; 
+import { CardActionArea, CardHeader, Grid, CardMedia } from '@material-ui/core/'; 
 import {observer} from 'mobx-react';
 import {withStyles} from '@material-ui/core/styles';
 import {
@@ -11,76 +11,80 @@ import {
 import { Divider } from '@material-ui/core';
 
 const styles = (theme: any) => ({
-    header: {
-      paddingLeft: theme.spacing(2),
-      height: '130px',
-    },
-    icon: {
-      paddingLeft: theme.spacing(3),
-      marginTop: theme.spacing(2),
-      height: '90px'
-    },
-    buttonDetail: {
-      paddingLeft: theme.spacing(2),
-      marginTop: theme.spacing(1),
-      padding: theme.spacing(0),
-    },
-    card:{
-      height: 180,
-      width: 400,
-      border: "2px",
-    },
-    fixedHeight: {
-      height: 180,
-      width: 400,
-    },
-    cardContent:{
-      padding: theme.spacing(0),
-    }
-  });
+  cardMedia: {
+    marginLeft: theme.spacing(4),
+    marginRight: theme.spacing(4),
+    marginBottom: theme.spacing(2)
+  },
+  cardButton: {
+    height: '3vh'
+  },
+  fixedHeight: {
+    maxWidth: '550px',
+    width: "25vw",
+  },
+  cardContent:{
+    padding: theme.spacing(0),
+  },
+});
 
-const ActionTestCard = observer( (props: any) => {
-    const {classes} = props;
-    const navigate = useNavigate();
-    return (
-        <Card variant="outlined" className={classes.fixedHeight}>
-        <CardActionArea onClick={() => navigate(props.route)} className={classes.fixedHeight}>
-            <CardHeader
-            title="Shrimp and Chorizo Paella"
-            subheader="September 14, 2016"
-            />
-            <CardContent className={classes.cardContent}>
-            <Grid direction="row"
-                  container 
-                  spacing={0}
-                  >
-                    <Grid item xs={8} md={8} lg={8} xl={8} className={classes.header}>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {props.header}
-                        </Typography>
-                        <Typography color="inherit" variant="body2">
-                            {props.text}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={4} md={4} lg={4} xl={4} className={classes.icon}>
-                        {props.icon}
-                    </Grid>
-                  </Grid>
-            <Divider/>
-            <Grid direction="row"
-                  container 
-                  spacing={0}
-                  >
-            <Grid item xs={12} md={12} lg={12} xl={12} className={classes.buttonDetail}>
-                        <Typography variant="button" color="primary" component="div">
-                            {props.buttonText}
-                        </Typography>
-                    </Grid>
-            </Grid>
-            </CardContent>
-        </CardActionArea>
-        </Card>
-    );
+interface ActionTestCardProps {
+  header: string;
+  text: string;
+  img: string;
+  buttonText: string;
+  handleTestClick: (test: string) => void;
+  classes: any;
+}
+
+const ActionTestCard = observer( (props: ActionTestCardProps) => {
+  const {classes} = props;
+
+  return (
+    <Card variant="outlined" className={classes.fixedHeight}>
+    <CardActionArea onClick={() => props.handleTestClick(props.header)} className={classes.fixedHeight}>
+    <CardHeader
+      title={props.header}
+      subheader={props.text}
+      style={{ textAlign: 'center', padding: 8}}
+      />
+      <CardContent className={classes.cardContent}>
+      <Grid 
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        container 
+        spacing={0}
+      >
+        <Grid item xs={12} md={12} lg={12} xl={12} className={classes.cardMedia}>
+        <CardMedia
+          component="img"
+          height="100%"
+          image={props.img}
+          alt="image"
+        />
+        </Grid>
+      </Grid>
+      <Divider/>
+      <Grid 
+        direction="row"
+        container 
+        alignItems="center"
+        justifyContent="center"
+        className={classes.cardButton}
+        spacing={0}
+      >
+      <Grid item xs={12} md={12} lg={12} xl={12}>
+      
+            <Typography style={{ textAlign: 'center', margin: 'auto'}} variant="button" color="primary" component="div">
+                {props.buttonText}
+            </Typography>
+        </Grid>
+      </Grid>
+      </CardContent>
+    </CardActionArea>
+    </Card>
+  );
 });
 
 export default withStyles(styles)(ActionTestCard);
--- src/components/views/Tests.tsx
+++ src/components/views/Tests.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {observer} from 'mobx-react';
 import {withStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -9,36 +9,14 @@ import {
 } from "react-router-dom";
 import { authenticate } from '../Communicator';
 import  ActionTestCard from '../ActionTestCard'
-import PeopleIcon from '@material-ui/icons/People';
-import PieChartIcon from '@material-ui/icons/PieChart';
-import AssignmentIcon from '@material-ui/icons/Assignment';
+import MotionDots from '../../files/motiondots.jpg';
+import FormFixed from '../../files/formfixed50.jpg';
+import FormRandom from '../../files/formrandom50.jpg';
+import FormDialog from '../FormDialog';
 
 const styles = (theme: any) => ({
-  root: {
-    display: 'flex',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
   container: {
-    paddingTop: theme.spacing(6),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column' as "column",
-  },
-  fixedHeight: {
-    height: 180,
-    width: 400,
+    marginTop: theme.spacing(10),
   },
 });
 
@@ -51,10 +29,9 @@ const styles = (theme: any) => ({
 const Tests = observer( (props: any) => {
   const {classes} = props;
   const [cookies, setCookie] = useCookies(['c_user']);
+  const [open, setOpen] = useState(false);
+  const [test, setTest] = useState('Not set');
   const navigate = useNavigate();
-  const iconStyles = {
-    fontSize: '82px',
-  };
 
   useEffect(() => {
     const authFunction = async () => {
@@ -68,56 +45,55 @@ const Tests = observer( (props: any) => {
     }
     authFunction();
   }, []);
-  
-    return (
-      
-      <div>
-        <div className={classes.appBarSpacer} />
-          <Container maxWidth="xl" className={classes.container}>
-            <Grid direction="column"
-                  alignItems="center"
-                  justifyContent="center" 
-                  container 
-                  spacing={5}
-                  >
-              <Grid direction="row"
-                    alignItems="center"
-                    justifyContent="center" 
-                    container
-              >
-                <Grid item xs={4} md={4} lg={4} xl={4}>
-                  <ActionTestCard 
-                      header={"Start en test"} 
-                      text={"Lar deg velge og starte forskjellige tester."} 
-                      buttonText={"Se og start tester"}
-                      icon={<AssignmentIcon color="primary" style={iconStyles} />}
-                      route={"/tests"}>
-                  </ActionTestCard>
-                </Grid>
-                <Grid item xs={4} md={4} lg={4} xl={4}>
-                  <ActionTestCard 
-                      header={"Elevoversikt"} 
-                      text={"Se en oversikt over elevene ved skolen din og " +
-                      "resultatene deres, eller legg til eller fjern elever fra oversikten. "} 
-                      buttonText={"Se oversikt over elever"}
-                      icon={<PeopleIcon color="primary" style={iconStyles} />}
-                      route={"/students"}>
-                  </ActionTestCard>
-                </Grid>
-              </Grid>
-              <Grid item xs={4} md={4} lg={4} xl={4}>
-                <ActionTestCard 
-                    header={"Siste resultater"} 
-                    text={"Se resultatene fra de siste testene som har blitt gjennomført."} 
-                    buttonText={"Se nye resultater"}
-                    icon={<PieChartIcon color="primary" style={iconStyles} />}
-                    route={"/students/sort-by-date"}>
-                  </ActionTestCard>
-              </Grid>
+
+  function openDialog(test: string){
+    setTest(test);
+    setOpen(true);
+  }
+
+  return (
+    
+    <div>
+        <Container maxWidth="xl" className={classes.container}>
+          <Grid direction="row"
+              alignItems="center"
+              justifyContent="center" 
+              container 
+              spacing={8}
+            >
+            <Grid item xs={5} md={5} lg={5} xl={5}>
+              <ActionTestCard 
+                  header={"Motion Test"} 
+                  text={"Magno bevegelsestest tester den magnocellulære kapasiteten til eleven"} 
+                  img={MotionDots}
+                  handleTestClick={openDialog}
+                  buttonText={"Start test"}>
+              </ActionTestCard>
             </Grid>
-          </Container>
-      </div>
-    );
-  });
+            <Grid item xs={5} md={5} lg={5} xl={5}>
+              <ActionTestCard 
+                  header={"Fixed Form Test"} 
+                  text={"Magno fiksertformtest tester den magnocellulære kapasiteten til eleven"} 
+                  img={FormFixed}
+                  handleTestClick={openDialog}
+                  buttonText={"Start test"}>
+              </ActionTestCard>
+            </Grid>
+            <Grid item xs={5} md={5} lg={5} xl={5}>
+              <ActionTestCard 
+                  header={"Random Form Test"} 
+                  text={"Magno tilfeldigformtest tester den magnocellulære kapasiteten til eleven"} 
+                  img={FormRandom}
+                  handleTestClick={openDialog}
+                  buttonText={"Start test"}>
+              </ActionTestCard>
+            </Grid>
+            <Grid item xs={5} md={5} lg={5} xl={5}></Grid>
+          </Grid>
+          <FormDialog store={props.store} test={test} open={open} setOpen={setOpen}/>
+        </Container>
+    </div>
+  );
+});
 
 export default withStyles(styles)(Tests);
