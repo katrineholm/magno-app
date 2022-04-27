abc1a1de702df542d0aee595530f102fa8bbe489
Jack Syvertsen <jmsyvert@stud.ntnu.no>
2022-04-27 10:42:20 +0200

    Adds first version of search box for students

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,30 @@
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import React, { useState } from 'react';
+  
+const students = ["Jack", "Mari", "Anders", "Erik", "Thomas", "Ingrid", "Laila", "Bryan","Ola"
+];
+
+export default function SearchBox(props: any) {
+  const [value, setValue] = React.useState<string | null>();
+  const [inputValue, setInputValue] = React.useState('');
+
+    return (
+      <Autocomplete
+        ListboxProps={{ style: { maxHeight: 150, overflow: 'auto' } }}
+        disablePortal
+        id="search-box"
+        options={students}
+        value={value}
+        onChange={(event: any, newValue: string | null) => {
+          setValue(newValue);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        renderInput={(params) => <TextField {...params} label="Skriv inn navn" />}
+      />
+    );
+  }
+
