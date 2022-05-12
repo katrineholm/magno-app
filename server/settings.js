a14c14ae764e3fddb774f7a52ac68648a9c312ea
Jack Syvertsen <jmsyvert@stud.ntnu.no>
2022-05-12 14:06:54 +0200

    Add settings page with thresholds to set risk

---
+++ server/settings.js
@@ -0,0 +1,18 @@
+function settings() {
+    return ({
+        motion_test: {
+            threshold_high: 50,
+            threshold_medium: 28,
+        },
+        fixed_form_test:{
+            threshold_high: 50,
+            threshold_medium: 28,
+        },
+        random_form_test:{
+            threshold_high: 50,
+            threshold_medium: 28,
+        }
+    })
+}
+
+module.exports = settings
