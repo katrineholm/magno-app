89c32c04b8219bc2354daceeb6236b26e2d7264f
jacksyv <40327091+jacksyv@users.noreply.github.com>
2022-06-29 23:07:42 +0200

    Update db-account-config.js
    Remove key and endpoint

--- server/dbconfig/db-account-config.js
+++ server/dbconfig/db-account-config.js
@@ -1,7 +1,7 @@
 function db_account_config() {
     return ({
-        endpoint: "https://magno-platform-db.documents.azure.com:443/",
-        key: "YOWkipBzndjmzlZXT0Tlzuf83pEHd4kXYdmEznRlbIqxzr4Y9vqsaHZGv0k8DZWuQwYGAjh0OtRdEv2QRHJkRg==",
+        endpoint: "",
+        key: "",
         databaseId: "magno-database",
         containerId: "magno-container",
         partitionKey: { kind: "Hash", paths: ["/id"] }
