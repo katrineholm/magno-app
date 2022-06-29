005786059828d385e0bf677faebd2a213961c7f7
jacksyv <40327091+jacksyv@users.noreply.github.com>
2022-06-29 23:07:58 +0200

    Update db-student-config.js
    Remove key and endpoint

--- server/dbconfig/db-student-config.js
+++ server/dbconfig/db-student-config.js
@@ -1,7 +1,7 @@
 function db_student_config() {
     return ({
-        endpoint: "https://magno-platform-db.documents.azure.com:443/",
-        key: "YOWkipBzndjmzlZXT0Tlzuf83pEHd4kXYdmEznRlbIqxzr4Y9vqsaHZGv0k8DZWuQwYGAjh0OtRdEv2QRHJkRg==",
+        endpoint: "",
+        key: "",
         databaseId: "magno-database",
         containerId: "magno-student-container",
         partitionKey: { kind: "Hash", paths: ["/id"] }
