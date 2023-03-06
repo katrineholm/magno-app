34b1a04a6624ecd6f3feb728b0becacd301aa99d
Katrine Holm <Katrine@dhcp-10-24-138-49.wlan.ntnu.no>
2023-03-06 11:08:10 +0100

    built tests

--- server/dbconfig/db-account-config.js
+++ server/dbconfig/db-account-config.js
@@ -1,7 +1,7 @@
 function db_account_config() {
     return ({
-        endpoint: "",
-        key: "",
+        endpoint: "https://magno-database.documents.azure.com:443/",
+        key: "QNZK6vrmYjvIMmXaYw9VsSaQC4zpMbXP87nL5OIXo3oDpCa7RGNDvPYA0LhUFdXkruwn0CWf1mtUACDbVYVAgQ==",
         databaseId: "magno-database",
         containerId: "magno-user-container",
         partitionKey: { kind: "Hash", paths: ["/id"] }
