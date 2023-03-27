289f8305e49b673629edf9801f30f5f05e2c04b8
vaarslarssen <vaar.sl98@gmail.com>
2023-03-27 12:54:37 +0200

    Possible to set role when registering, and it is sent to the database

--- server/dbconfig/db-student-config.js
+++ server/dbconfig/db-student-config.js
@@ -1,7 +1,7 @@
 function db_student_config() {
     return ({
         endpoint: "https://magno-database.documents.azure.com:443/",
-        key: "QNZK6vrmYjvIMmXaYw9VsSaQC4zpMbXP87nL5OIXo3oDpCa7RGNDvPYA0LhUFdXkruwn0CWf1mtUACDbVYVAgQ==",
+        key: "",
         databaseId: "magno-database",
         containerId: "magno-student-container",
         partitionKey: { kind: "Hash", paths: ["/id"] }
