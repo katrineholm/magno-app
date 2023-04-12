975d72433f27cc6cdc6173c5d917daf5d7822950
Katrine <katrine.holm.norheim@gmail.com>
2023-04-12 12:15:06 +0200

    deleted old urls

--- src/components/urls.tsx
+++ src/components/urls.tsx
@@ -3,40 +3,22 @@
 /* base url */
 const baseURL = process.env.REACT_APP_API_URL;
 
-/* Login post, get, update */
-export const loginURL = `${baseURL}/login`;
-
+/* Log in */
 export const newloginURL = `${baseURL}/new-login`;
-export const newGetStudentsURL = `${baseURL}/new-get-students`;
 
-
-/* Logout post */
-export const logoutURL = `${baseURL}/logout`;
-
-/* Account post, get, update */
-export const accountURL = `${baseURL}/account`;
-
+/* Register user */
 export const newAccountURL = `${baseURL}/new-create-user`;
-
-export const newGetClassesURL = `${baseURL}/new-get-classes`;
-
-/* Authenticate post */
-export const authenticateURL = `${baseURL}/authenticate`;
 
 /* Get current user */
 export const getCurrentUserURL = `${baseURL}/new-get-current-user` // denne burde oppdateres:)
 
-/* Add student post */
-export const addStudentURL = `${baseURL}/addStudent`;
+/* Get students */
+export const newGetStudentsURL = `${baseURL}/new-get-students`;
 
 /* Add newstudent post */
 export const newAddStudentURL = `${baseURL}/new-add-student`;
 
-/* Get students */
-export const getStudentsURL = `${baseURL}/students`;
-
-/** Get classes  */
-export const getClassesURL = `${baseURL}/classes`;
+export const newGetClassesURL = `${baseURL}/new-get-classes`;
 
 /** Add class  */
 export const addClassURL = `${baseURL}/addClass`;
@@ -46,19 +28,12 @@ export const getTeachersURL = `${baseURL}/getTeachers`;
 
 
 export default {
-  account: accountURL,
   newAccount: newAccountURL,
-  login: loginURL,
   newLogin: newloginURL,
+  getCurrentUser: getCurrentUserURL,
   newGetStudents: newGetStudentsURL,
   newAddStudent: newAddStudentURL,
   newGetClasses: newGetClassesURL,
-  logout: logoutURL,
-  authenticate: authenticateURL,
-  getCurrentUser: getCurrentUserURL,
-  addStudent: addStudentURL,
-  getStudents: getStudentsURL,
-  getClasses: getClassesURL,
   addClass: addClassURL,
   getTeachers: getTeachersURL
 };
