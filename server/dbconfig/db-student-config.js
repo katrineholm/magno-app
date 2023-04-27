b9a667eb78005f359ee3719d6b91961fe1294c67
vaarslarssen <vaar.sl98@gmail.com>
2023-04-27 17:03:16 +0200

    removed user secrets hehe

--- server/dbconfig/db-student-config.js
+++ server/dbconfig/db-student-config.js
@@ -1,7 +1,7 @@
 function db_student_config() {
     return ({
         endpoint: "https://magno-database.documents.azure.com:443/",
-        key: "gun3nnXNSMwdQXga9LnYjUwdV1SEtWRYIE5U3QVeO2HyBV2OvPAwqLhAm1zFOPrrbJ0F8vwQAIGEACDb6dawsg==",
+        key: "",
         databaseId: "magno-database",
         containerId: "magno-student-container",
         partitionKey: { kind: "Hash", paths: ["/id"] }
